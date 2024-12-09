37a5fb5ac783f99fb81ef099974589ae81884109
navid bahrami <navidbahrami1996@gmail.com>
2024-12-09 13:34:19 +0330

    blog controller

---
+++ controller/blog.controller.js
@@ -0,0 +1,51 @@
+async function getAllBlogs(req , res , next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+async function createNewBlog(req , res , next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+async function removeBlog(req , res , next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+async function searchByTitle(req , res , next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+async function searchByMultiField(req , res , next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+async function searchByRegexp(req , res , next){
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
+}
+
+module.exports = {
+    getAllBlogs ,
+    createNewBlog ,
+    removeBlog ,
+    searchByTitle ,
+    searchByMultiField ,
+    searchByRegexp
+}

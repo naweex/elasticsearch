28aa671152f61f7cc8bbd759e0303e5b9784cc6d
navid bahrami <navidbahrami1996@gmail.com>
2024-12-09 11:50:11 +0330

    index

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
+require('dotenv').config();
 const {PORT} = process.env;
 const expressEjsLayouts = require('express-ejs-layouts');
+const { AllRouters } = require('./router/router');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.set('view engine' , 'ejs');
 app.use(expressEjsLayouts);
+app.set('views' , 'views')
+app.set('layout' , './layouts/master')
+app.use(AllRouters)
+app.use((req , res , next) => {
+    return res.status(404).json({
+        status : 404,
+        message : 'not found'
+    })
+})
+app.use((req , res , err , next) => {
+    return res.status(err.status || 500).json({
+        status : err.status || 500,
+        message : err.message
+    })
+})
+
+server.listen(PORT , () =>{
+    console.log(`server run at : http://localhost:${PORT}`);
+    
+})

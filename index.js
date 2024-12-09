2424d2ae4973880b4a15246f37fc6066903d6f20
navid bahrami <navidbahrami1996@gmail.com>
2024-12-09 10:57:59 +0330

    index

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const {PORT} = process.env;
+const expressEjsLayouts = require('express-ejs-layouts');
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.set('view engine' , 'ejs');
+app.use(expressEjsLayouts);

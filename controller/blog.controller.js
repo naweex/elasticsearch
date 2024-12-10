fa7b660e75d4812913bff9b657e97b297fe2d298
navid bahrami <navidbahrami1996@gmail.com>
2024-12-10 16:27:45 +0330

    blog controller

--- controller/blog.controller.js
+++ controller/blog.controller.js
@@ -1,8 +1,16 @@
 const { elasticClient } = require("../config/elastic.config");
+const {EventEmitter} = require("events")
+const { resourceLimits } = require("worker_threads")
+const event = new EventEmitter()
 const indexBlog = 'blog'
 async function getAllBlogs(req , res , next){
     try {
-        
+        const value = req.params.value;
+        const blogs = await elasticClient.search({
+            index : indexBlog ,
+            q : value
+        })
+        return res.json(blogs.hits.hits)
     } catch (error) {
         next(error)
     }
@@ -23,11 +31,47 @@ async function createNewBlog(req , res , next){
         next(error)
     }
 }
+async function saveToElastic(){
+    event.on("saveToElastic", (finalData) => {
+        elasticClient.index({
+            index: indexBlog, 
+            document : finalData
+        })
+    })
+}
 async function removeBlog(req , res , next){
     try {
-        
+        const {id} = req.params;
+        const deletedResult = await elasticClient.deleteByQuery({
+            index : indexBlog ,
+            query : {
+                match : {
+                    _id : id
+                }
+            }
+        })
+        return res.json(deletedResult)
     } catch (error) {
         next(error)
+    }
+}
+async function updateBlog(req , res , next){
+    try {
+        const {id} = req.params;
+        const data = req.body;
+        Object.keys(data).forEach(key => {
+            if(!data[key]) delete data[key]
+        })
+        const blog = (await elasticClient.search({index: indexBlog, query: {match: {_id: id}}})).hits.hits?.[0] || {};
+        const payload = blog?._source || {}
+        const updateResult = await elasticClient.index({
+            index: indexBlog,
+            id,
+            body: {...payload, ...data}
+        })
+        return res.json(updateResult)
+    } catch (error) {
+        
     }
 }
 async function searchByTitle(req , res , next){
@@ -58,5 +102,6 @@ module.exports = {
     removeBlog ,
     searchByTitle ,
     searchByMultiField ,
-    searchByRegexp
+    searchByRegexp ,
+    updateBlog
 }

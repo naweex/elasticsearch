daa94dc62ef0f3df792d209bb032f4f31eb50051
navid bahrami <navidbahrami1996@gmail.com>
2024-12-11 11:42:09 +0330

    blog router

--- router/blog.routes.js
+++ router/blog.routes.js
@@ -1,9 +1,13 @@
-const { createNewBlog } = require('../controller/blog.controller')
+const { createNewBlog, getAllBlogs, removeBlog, updateBlog , updateBlog2, searchByTitle } = require('../controller/blog.controller')
 
 const blogRouter = require('express').Router()
 
 blogRouter.post('/create' , createNewBlog)
-
+blogRouter.get('/list/:value?' , getAllBlogs)
+blogRouter.delete('/delete/:id' , removeBlog)
+blogRouter.put('/update/:id' , updateBlog)
+blogRouter.put('/update2/:id' , updateBlog2)
+blogRouter.get('/findByTitle' , searchByTitle)
 module.exports = {
     blogRouter
 }
